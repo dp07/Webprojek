e5f2473c02f7aaef4ba4ce194fa9ae97f1bd9fa0
dp07 <dp.madridiztazc@gmail.com>
2019-10-26 17:27:54 +0700

    anu

--- registrasi.php
+++ registrasi.php
@@ -15,6 +15,11 @@
         <?php if(@$_GET['w'])
         {echo'<script>alert("'.$_GET['w'].'");</script>';}
         ?>
+        <script>
+            function validateForm() {var y = document.forms["form"]["name"].value;	var letters = /^[A-Za-z]+$/;if (y == null || y == "") {alert("Name must be filled out.");return false;}var z =document.forms["form"]["college"].value;if (z == null || z == "") {alert("college must be filled out.");return false;}var x = document.forms["form"]["email"].value;var atpos = x.indexOf("@");
+            var dotpos = x.lastIndexOf(".");if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {alert("Not a valid e-mail address.");return false;}var a = document.forms["form"]["password"].value;if(a == null || a == ""){alert("Password must be filled out");return false;}if(a.length<5 || a.length>25){alert("Passwords must be 5 to 25 characters long.");return false;}
+            var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords must match.");return false;}}
+        </script>
 </head>
 <body>
       <div class="header">
@@ -38,49 +43,130 @@
       
       <form class="form-horizontal" action="login.php?q=index.php" method="POST">
       <fieldset>
-
-      <!-- Text input-->
-      <div class="form-group">
+    <!-- Text input-->
+    <div class="form-group">
       <label class="col-md-3 control-label" for="email"></label>  
-          <div class="col-md-6">
-              <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
-          </div>
+      <div class="col-md-6">
+      <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
+        
       </div>
+    </div>
 
-      <!-- Password input-->
-      <div class="form-group">
+
+    <!-- Password input-->
+    <div class="form-group">
       <label class="col-md-3 control-label" for="password"></label>
-          <div class="col-md-6">
-            <input id="password" name="password" placeholder="Enter your Password" class="form-control input-md" type="password">
+      <div class="col-md-6">
+        <input id="password" name="password" placeholder="Enter your Password" class="form-control input-md" type="password">
+        
+      </div>
+    </div>
+
           </div>
-      </div>
-      </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Log in</button>
+    		</fieldset>
+    </form>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+    <!--sign in modal closed-->
 
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Log in</button>
-		  </fieldset>
-      </form>
-      </div>
-              </div><!-- /.modal-content -->
-          </div><!-- /.modal-dialog -->
-      </div><!-- /.modal -->
-      <!--sign in modal closed-->
-      </div><!--header row closed-->
-      </div>
+    </div><!--header row closed-->
+    </div>
 
-      <div class="bg1">
-      <div class="row">
-      <div class="col-md-5"></div>
-      <div class="col-md-4" style="margin-top: 190px;">
-        <button class="btn btn-warning font-weight-bold" style="height: 50px; font-size: 20px; font-weight: bold;">
-          <a style=" color: #000;" href="registrasi.php">REGISTRASI</a>
-        </button>
+<div class="bg1">
+<div class="row">
 
-      </div>
-  
+<div class="col-md-4"></div>
+<div class="col-md-4 panel">
+
+<!-- sign in form begins -->  
+  <form class="form-horizontal" name="form" action="sign.php?q=account.php" onSubmit="return validateForm()" method="POST">
+<fieldset>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="name"></label>  
+  <div class="col-md-12">
+  <input id="name" name="name" placeholder="Enter your name" class="form-control input-md" type="text">
+    
+  </div>
 </div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="gender"></label>
+  <div class="col-md-12">
+    <select id="gender" name="gender" placeholder="Enter your gender" class="form-control input-md" >
+   <option value="Male">Select Gender</option>
+  <option value="M">Male</option>
+  <option value="F">Female</option> </select>
+  </div>
 </div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="name"></label>  
+  <div class="col-md-12">
+  <input id="college" name="college" placeholder="Enter your college name" class="form-control input-md" type="text">
+    
+  </div>
+</div>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label title1" for="email"></label>
+  <div class="col-md-12">
+    <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="mob"></label>  
+  <div class="col-md-12">
+  <input id="mob" name="mob" placeholder="Enter your mobile number" class="form-control input-md" type="number">
+    
+  </div>
+</div>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="password"></label>
+  <div class="col-md-12">
+    <input id="password" name="password" placeholder="Enter your password" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-12control-label" for="cpassword"></label>
+  <div class="col-md-12">
+    <input id="cpassword" name="cpassword" placeholder="Conform Password" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+<?php if(@$_GET['q7'])
+{ echo'<p style="color:red;font-size:15px;">'.$_GET['q7'];}?>
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-12 control-label" for=""></label>
+  <div class="col-md-12"> 
+    <input  type="submit" class="sub" value="sign up" class="btn btn-primary"/>
+  </div>
+</div>
+
+</fieldset>
+</form>
+</div><!--col-md-6 end-->
+</div></div>
 </div><!--container end-->
 
 <!--Footer start-->
