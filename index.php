898ba027ade615679ab28142b0be22bde9fd8b44
dp07 <dp.madridiztazc@gmail.com>
2019-10-28 22:19:13 +0700

    anu

--- index.php
+++ index.php
@@ -13,17 +13,27 @@
       <script src="js/bootstrap.min.js"  type="text/javascript"></script>
  	    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
         <?php if(@$_GET['w'])
-        {echo'<script>alert("'.$_GET['w'].'");</script>';}
+        {echo'<script>alert("'.@$_GET['w'].'");</script>';}
         ?>
+        <script>
+            function validateForm() {var y = document.forms["form"]["name"].value;  var letters = /^[A-Za-z]+$/;if (y == null || y == "") {alert("Name must be filled out.");return false;}var z =document.forms["form"]["college"].value;if (z == null || z == "") {alert("college must be filled out.");return false;}var x = document.forms["form"]["email"].value;var atpos = x.indexOf("@");
+            var dotpos = x.lastIndexOf(".");if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {alert("Not a valid e-mail address.");return false;}var a = document.forms["form"]["password"].value;if(a == null || a == ""){alert("Password must be filled out");return false;}if(a.length<5 || a.length>25){alert("Passwords must be 5 to 25 characters long.");return false;}
+            var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords must match.");return false;}}
+        </script>
 </head>
 <body>
       <div class="header">
       <div class="row">
         <div class="col-lg-6">
             <span class="logo">Ujian Online</span>
+            <div class="header2">
+             <a href="#" target="_blank">About</a>
+             <a href="#" target="_blank">Pesan</a>
+            </div>
         </div>
       <div class="col-md-2 col-md-offset-4">
           <a href="#" class="pull-right btn sub1" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Signin</b></span></a>
+      </div>
       </div>
 
       <!--sign in modal start-->
@@ -55,7 +65,8 @@
           </div>
       </div>
       </div>
-
+      <?php if(@$_GET['q7'])
+      { echo'<p style="color:red;font-size:15px;">'.@$_GET['q7'];}?>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         <button type="submit" class="btn btn-primary">Log in</button>
@@ -71,30 +82,110 @@
 
       <div class="bg1">
       <div class="row">
-      <div class="col-md-5"></div>
+     <!--  <div class="col-md-5"></div>
       <div class="col-md-4" style="margin-top: 190px;">
         <button class="btn btn-warning font-weight-bold" style="height: 50px; font-size: 20px; font-weight: bold;">
           <a style=" color: #000;" href="registrasi.php">REGISTRASI</a>
         </button>
 
+      </div> -->
+      <div class="col-md-3"></div>
+      <div class="col-md-4 panel">
+
+      <!-- sign in form begins -->  
+        <form class="form-horizontal" name="form" action="sign.php?q=account.php" onSubmit="return validateForm()" method="POST">
+      <fieldset>
+
+
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-12 control-label" for="name"></label>  
+        <div class="col-md-12">
+        <input id="name" name="name" placeholder="Enter your name" class="form-control input-md" type="text">
+          
+        </div>
       </div>
-  
-</div>
-</div>
-</div><!--container end-->
+
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-12 control-label" for="gender"></label>
+        <div class="col-md-12">
+          <select id="gender" name="gender" placeholder="Enter your gender" class="form-control input-md" >
+         <option value="Male">Select Gender</option>
+        <option value="M">Male</option>
+        <option value="F">Female</option> </select>
+        </div>
+      </div>
+
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-12 control-label" for="name"></label>  
+        <div class="col-md-12">
+        <input id="college" name="college" placeholder="Enter your college name" class="form-control input-md" type="text">
+          
+        </div>
+      </div>
+
+
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-12 control-label title1" for="email"></label>
+        <div class="col-md-12">
+          <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
+          
+        </div>
+      </div>
+
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-12 control-label" for="mob"></label>  
+        <div class="col-md-12">
+        <input id="mob" name="mob" placeholder="Enter your mobile number" class="form-control input-md" type="number">
+          
+        </div>
+      </div>
+
+
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-12 control-label" for="password"></label>
+        <div class="col-md-12">
+          <input id="password" name="password" placeholder="Enter your password" class="form-control input-md" type="password">
+          
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label class="col-md-12control-label" for="cpassword"></label>
+        <div class="col-md-12">
+          <input id="cpassword" name="cpassword" placeholder="Conform Password" class="form-control input-md" type="password">
+          
+        </div>
+      </div>
+      <?php if(@$_GET['q7'])
+      { echo'<p style="color:red;font-size:15px;">'.@$_GET['q7'];}?>
+      <!-- Button -->
+      <div class="form-group">
+        <label class="col-md-12 control-label" for=""></label>
+        <div class="col-md-12"> 
+          <input type="submit" class="sub" value="Daftar"/>
+        </div>
+      </div>
+
+      </fieldset>
+      </form>
+      </div><!--col-md-6 end-->
+      </div></div>
+      </div><!--container end-->
 
 <!--Footer start-->
 <div class="row footer">
 <div class="col-md-3 box">
-<a href="http://www.projectworlds/online-examination" target="_blank">About us</a>
+<a style="font-weight:bold; " href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
+<div class="col-md-3 box">
+<a style="font-weight:bold;  href="#" data-toggle="modal" data-target="#developers">Developers</a>
 </div>
-<div class="col-md-3 box">
-<a href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
-<div class="col-md-3 box">
-<a href="#" data-toggle="modal" data-target="#developers">Developers</a>
 </div>
-<div class="col-md-3 box">
-<a href="feedback.php" target="_blank">Feedback</a></div></div>
 <!-- Modal For Developers-->
 <div class="modal fade title1" id="developers">
   <div class="modal-dialog">
@@ -111,10 +202,10 @@
 		 <img src="image/CAM00121.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
 		 </div>
 		 <div class="col-md-5">
-		<a href="http://yugeshverma.blogspot.in" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">Yugesh Verma</a>
-		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+91 9165063741</h4>
-		<h4 style="font-family:'typo' ">vermayugesh323@gmail.com</h4>
-		<h4 style="font-family:'typo' ">Chhattishgarh insitute of management & Technology ,bhilai</h4></div></div>
+		<a href="#" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">Dany Pradana</a>
+		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+62 xxxxxxxxx</h4>
+		<h4 style="font-family:'typo' ">danyparadana@gmail.com</h4>
+		<h4 style="font-family:'typo' ">Universitas Negeri Semarang</h4></div></div>
 		</p>
       </div>
     
